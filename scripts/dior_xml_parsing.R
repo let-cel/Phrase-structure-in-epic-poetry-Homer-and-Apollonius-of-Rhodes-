2c2cb7df8b5c0a3c9a6dbd7b628cd7339d8e9a6d
locusclassicus <alieva.mgl@gmail.com>
2024-05-16 22:24:21 +0300

    dior_xml_parsing.R: filepath fixed, libraries attached

--- scripts/dior_xml_parsing.R
+++ scripts/dior_xml_parsing.R
@@ -1,4 +1,9 @@
-doc_d <- xmlTreeParse("Iliad_Diorisis.xml", useInternalNodes = T)
+library(XML)
+library(dplyr)
+library(purrr)
+
+url <- "./XML/Iliad_dior.xml"
+doc_d <- xmlTreeParse(url, useInternalNodes = T)
 
 # get rootnode
 rootnode <- xmlRoot(doc_d)
@@ -9,9 +14,10 @@ word_nodes <- getNodeSet(rootnode,
                          "//word", 
                          namespaces = c(tei = ns))
 
-word = map(word_nodes, xmlGetAttr, "form") %>% as.character()
-location <- map(word_nodes, ~xmlGetAttr(xmlParent(.), "location")) %>% as.character()
-
+word <-  map(word_nodes, xmlGetAttr, "form") %>% 
+  as.character()
+location <- map(word_nodes, ~xmlGetAttr(xmlParent(.), "location")) %>% 
+  as.character()
 
 lemma_nodes <- map(word_nodes, ~xmlChildren(.)$lemma)
 pos <-  map(lemma_nodes, xmlGetAttr, "POS") %>% as.character()
